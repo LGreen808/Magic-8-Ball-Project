c7528e14f174e4c3c2d9204ad20760e67f7821a2
526LGreen@frhsd.com <526LGreen@frhsd.com>
2024-11-26 11:48:24 -0500

    Working pt 3

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
 //get username
-let userName =prompt('What\'s your name?');
+let userName = prompt('Hello, What is your name?')
+
+let responseImg = document.getElementById('response-img')
+let responseText = document.getElementById('response-text')
+let greeting = document.getElementById('greeting')
 //Input username to website
 userName
-? (greeting.innerText=`Hello, ${username}!`)
-: (greeting.innerText='Hello stranger!');
+? (greeting.innerText = `Hello, ${userName}!`)
+: (greeting.innerText = 'Hello Stranger!')
+function shakeMagic8Ball () {
+//Get User Question
+let userQuestion = prompt('What do you want to ask the 8 Ball?');
+//Do Not Touch This Code!
 
-
-userName ? console.log(`Hello, ${userName}!`)
-: console.log('Hello!');
-const userQuestion = ('Will I do good on my English Test?')
-console.log(userQuestion);
-const randomNumber = Math.floor(Math.random() * 8);
+let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = ('');
+let imageChoice = (''); 
 switch (randomNumber) {
-  case 0:
-    eightBall = 'It is certain';
-    break;
-  case 1:
-    eightBall = 'It is decidedly so';
-  break;
-  case 2: 
-    eightBall = 'Reply hazy try again';
-    break;
-  case 3:
-   eightBall = 'Cannot predict now';
-    break;
-  case 4:
-   eightBall = 'Do not count on it';
-    break;
-  case 5:
-    eightBall = 'My sources say no';
-    break;
- case 6:
-    eightBall = 'Outlook not so good';
-    break;
-  case 7:
-    eightBall = 'Signs point to yes';
-    break;
-  default: 
-     eightBall = 'YES';
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+    default:
+        eightBall = 'YES';
 }
 
 console.log(eightBall);
+responseText.innerText = `${userName} has asked ${userQuestion} to the almighty 8 Ball. The 8 ball he replied ${eightBall}!`
+responseImg.src = imageChoice
 
+}
